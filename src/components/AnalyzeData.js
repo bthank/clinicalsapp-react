97423d576ad854b5bef0e27290c2489973ec729e
bthank <bthankachan@yahoo.com>
2021-08-22 19:14:52 -0400

    Added components for the app

---
+++ src/components/AnalyzeData.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class AnalyzeData extends React.Component {
+
+    render(){
+        /* the code within the render return is JSX code which gets converted to HTML
+        JSX is a syntax extension to JavaScript
+        */
+        return (<div>
+
+        </div>);
+    }
+
+}
+
+/* export out the component so that it can be used in other components */
+export default AnalyzeData;
