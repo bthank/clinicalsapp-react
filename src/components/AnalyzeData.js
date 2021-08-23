e9c4371c3e811965fd0957d27e5920b54cf5b060
bthank <bthankachan@yahoo.com>
2021-08-22 21:18:55 -0400

    Implement Analyze component

--- src/components/AnalyzeData.js
+++ src/components/AnalyzeData.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import axios from 'axios';
 
 class AnalyzeData extends React.Component {
+
+    state = {
+        clinicalData: []
+    }
+
+    componentWillMount(){
+        axios.get("/clinicalservices/api/patients/analyze/" + this.props.match.params.patientId)
+            .then(response => {
+
+            })
+    }
 
     render(){
         /* the code within the render return is JSX code which gets converted to HTML
         JSX is a syntax extension to JavaScript
         */
         return (<div>
-
-        </div>);
+                    <h2>Patient Details:</h2>
+                    First Name: {this.state.firstName}
+                    Last Name: {this.state.lastName}
+                    Age: {this.state.age}
+                </div>);
     }
 
 }
