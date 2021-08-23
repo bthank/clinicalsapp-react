68fd9634f1db73cb84db26b7c4e902ca4ce2902c
bthank <bthankachan@yahoo.com>
2021-08-22 20:45:08 -0400

    Implemented Add Clinical Data component

--- src/components/CollectClinicals.js
+++ src/components/CollectClinicals.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import axios from 'axios';
 
 class CollectClinicals extends React.Component {
+
+    /* set state initially to a blank object since we will only get a single
+       patient object back */
+    state = {};
+
+    componentWillMount(){
+        axios.get("/clinicalservices/api/patients/" + this.props.match.params.patientId)
+            .then(response => {
+                this.setState(response.data);
+            })
+    }
 
     render(){
         /* the code within the render return is JSX code which gets converted to HTML
         JSX is a syntax extension to JavaScript
         */
         return (<div>
-
-        </div>);
+                    <h2>Patient Details:</h2>
+                    First Name: {this.state.firstName}
+                    Last Name: {this.state.lastName}
+                    Age: {this.state.age}
+                    <form>
+                        
+                    </form>
+                 </div>);
     }
 
 }
